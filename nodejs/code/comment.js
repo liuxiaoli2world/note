2644cbb7b5170259d85baad71aca11041d0fd278
liuxiaoli2world <liuxiaoli2world@gmail.com>
2019-01-23 21:08:16 +0800

    评论小实例

---
+++ nodejs/code/comment.js
@@ -0,0 +1,48 @@
+var http = require('http');
+var fs = require('fs');
+var url = require('url');
+var template = require('art-template');
+
+var comments = [
+  { message: '评论一，幅度萨芬撒旦发。', date: '2019-01-18' },
+  { message: '评论一，幅度萨芬撒旦发。', date: '2019-01-18' },
+  { message: '评论一，幅度萨芬撒旦发。', date: '2019-01-18' },
+  { message: '评论一，幅度萨芬撒旦发。', date: '2019-01-18' },
+  { message: '评论一，幅度萨芬撒旦发。', date: '2019-01-18' }
+];
+
+http
+  .createServer(function(req, res) {
+    var obj = url.parse(req.url, true);
+    var pathname = obj.pathname;
+    if (pathname === '/') {
+      fs.readFile('./html/template.html', function(err, data) {
+        if (err) {
+          res.end(err);
+        } else {
+          var htmlStr = template.render(data.toString(), comments);
+          res.end(htmlStr);
+        }
+      });
+    } else if (pathname === '/comment') {
+      var message = obj.query.message;
+      comments.push({ message: message, date: '2019-01-23' });
+
+      res.statusCode = 302;
+      res.setHeader('Location', '/');
+      res.end();
+    } else if (pathname === '/post.html') {
+      fs.readFile('./html/post.html', function(err, data) {
+        if (err) {
+          res.end(err);
+        } else {
+          res.end(data);
+        }
+      });
+    } else {
+      res.end('404');
+    }
+  })
+  .listen(3000, function() {
+    console.log('服务器启动成功！');
+  });
